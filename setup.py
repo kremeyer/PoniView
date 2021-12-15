4862570a6dbeb2440d53dd1e6000b72ece12ac0c
Laurenz Kremeyer <laurenz.kremeyer@uni-due.de>
2021-12-15 03:19:37 +0100

    automatically install requirements

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
     version='0.1',
     packages=find_packages(),
     include_package_data=True,
+    install_requires=['numpy', 'pyqtgraph', 'PyQt5', 'pyFAI', 'Pillow'],
     url='https://github.com/kremeyer/py_PoniView',
     license='',
     author='Laurenz Kremeyer',
